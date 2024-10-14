ebd40104904ff7a0bb247b62a310f1e3cfaa17f3
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-10-14 00:13:33 -0300

    feat (Exercicio1160): Population Increase

--- Problem1160.java
+++ Problem1160.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Problem1160 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int testCases = sc.nextInt();
 
