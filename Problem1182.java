0338d3e3c8b65029a7fef20d23efb32f74470dd5
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-10-25 17:39:28 -0300

    feat (Exercicio1182): Column in Array

---
+++ Problem1182.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Problem1182 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float[][] matrix = new float[12][12];
+        float sum = 0;
+
+        int n = sc.nextInt();
+        char operation = Character.toUpperCase(sc.next().charAt(0));
+
+        for (int row = 0; row < 12; row++) {
+            for (int col = 0; col < 12; col++) {
+                matrix[row][col] = sc.nextFloat();
+            }
+        }
+
+        for (int row = 0; row < 12; row++) {
+            sum += matrix[row][n];
+        }
+
+        if (operation == 'S') {
+            System.out.printf("%.1f%n", sum);
+        } else if (operation == 'M') {
+            System.out.printf("%.1f%n", sum / 12);
+        }
+    }
+}
