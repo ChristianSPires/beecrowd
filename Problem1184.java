b18af561017270609fa00349be2fa27eba55af2b
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-10-29 00:07:48 -0300

    feat (Exercicio1184): Below the Main Diagonal

---
+++ Problem1184.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Problem1184 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float sum = 0;
+        float[][] M = new float[12][12];
+
+        char operation = Character.toUpperCase(sc.next().charAt(0));
+
+        for (int row = 0 ; row < 12; row++) {
+            for (int col = 0 ; col < 12 ; col++) {
+                M[row][col] = sc.nextFloat();
+            }
+        }
+
+        for (int row = 1; row < 12; row++) {
+            for (int col = 0; col < row; col++) {
+                sum += M[row][col];
+            }
+        }
+
+        if (operation == 'S') {
+            System.out.printf("%.1f%n", sum);
+        } else if (operation == 'M') {
+            System.out.printf("%.1f%n", sum / 66);
+        }
+    }
+}
