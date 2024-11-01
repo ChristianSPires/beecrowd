fa0e610b768eaf0d5786cd74a56c6b57b49e2fe9
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-10-31 23:48:02 -0300

    feat (Exercicio1188): Inferior Area

---
+++ Problem1188.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Problem1188 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[][] M = new double[12][12];
+        double sum = 0;
+
+        char O = Character.toUpperCase(sc.next().charAt(0));
+
+        for (int row = 0; row < 12; row++) {
+            for (int col = 0; col < 12; col++) {
+                M[row][col] = sc.nextDouble();
+            }
+        }
+
+        for (int row = 0; row < 12; row++) {
+            for (int col = 0; col < 12; col++) {
+                if(row > col && row > 12 - col - 1) {
+                    sum += M[row][col];
+                }
+            }
+        }
+
+        if(O == 'M') sum /= 30;
+        System.out.printf("%.1f%n", sum);
+
+        sc.close();
+    }
+}
