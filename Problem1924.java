01b59a668cdd62cd6fa2ccc75fb1d24cc1ec822d
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-12-12 00:26:45 -0300

    feat (Problem1924): Vitória and Her Indecision

---
+++ Problem1924.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem1924 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            String course = sc.next();
+        }
+
+        System.out.println("Ciencia da Computacao");
+
+        sc.close();
+    }
+}
